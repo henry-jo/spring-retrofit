9d236db4b1dedda370381f321c7e7d985e6a7322
henry <henry@lendit.co.kr>
2019-09-24 21:16:32 +0900

    init project

---
+++ src/main/java/com/henry/springretrofit/SpringRetrofitApplication.kt
@@ -0,0 +1,12 @@
+package com.henry.springretrofit
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringRetrofitApplication
+
+fun main(args: Array<String>) {
+
+    SpringApplication.run(SpringRetrofitApplication::class.java, *args)
+}
---
+++ src/test/java/com/henry/springretrofit/SpringRetrofitApplicationTests.java
@@ -0,0 +1,15 @@
+package com.henry.springretrofit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRetrofitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
